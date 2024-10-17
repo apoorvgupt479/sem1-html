02b835c79e301e7736576bec4733fa98dfbe5a2e
Apoorv Gupta <64680081+apoorvgupt479@users.noreply.github.com>
2024-10-17 13:42:29 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function startGame1() {
+    alert("Game 1 is starting...");
+    // Add game-specific code here
+}
+
+function startGame2() {
+    alert("Game 2 is starting...");
+    // Add game-specific code here
+}
+
+function startGame3() {
+    alert("Game 3 is starting...");
+    // Add game-specific code here
+}
